30e7b30d4c61cc34a516001900f29e6f8dbf84e2
Alex Huchasheva <alexhuchasheva@gmail.com>
2018-09-25 15:46:26 +0300

    added table

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
     <div class="col-sm-4" style="text-align:right;"><?php dynamic_sidebar( 'footer-hours' ); ?></div> 
 </div> 
 <div class="container-fluid copyright">    
-<p class="text-center">&copy; <?php echo date('Y'); ?> CleanUp </p>
+<p class="text-center">&copy; <?php echo date('Y'); ?> Don't Worry </p>
     <p class="text-center" style="display:none;">Design & code: <a href="http://huchasheva-web.ru/">Alexandra Huchasheva</a></p>
 </div>
 
